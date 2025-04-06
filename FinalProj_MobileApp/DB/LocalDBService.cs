c9a1872d83f80dab7b0e4821f652f1ef68077de9
ZBetsch <betschzt@mail.uc.edu>
2025-04-05 20:52:02 -0400

    DB Service coded
    Crime.cs prepared
    
    Can now interface with database

--- FinalProj_MobileApp/DB/LocalDBService.cs
+++ FinalProj_MobileApp/DB/LocalDBService.cs
@@ -7,7 +7,41 @@ using SQLite;
 
 namespace FinalProj_MobileApp.DB
 {
-    internal class LocalDBService
+    public class LocalDBService
     {
+        private const string DB_NAME = "crime_db.db3";
+        private readonly SQLiteAsyncConnection _connection;
+
+        public LocalDBService()
+        {
+            _connection = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, DB_NAME));
+            _connection.CreateTableAsync<Crime>();
+        }
+
+        public async Task<List<Crime>> GetCrimes()
+        {
+            return await _connection.Table<Crime>().ToListAsync();
+        }
+
+        public async Task<Crime> GetById(int id)
+        {
+            return await _connection.Table<Crime>().Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task Create(Crime crime)
+        {
+            await _connection.InsertAsync(crime);
+        }
+
+        public async Task Update(Crime crime)
+        {
+            await _connection.UpdateAsync(crime);
+        }
+
+        public async Task Delete(Crime crime)
+        {
+            await _connection.DeleteAsync(crime);
+        }
+
     }
 }
