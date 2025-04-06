860084bd75263cbc3fee465fdd7610023d3e0d06
Zach Betsch <betschzt@mail.uc.edu>
2025-04-05 20:03:35 -0400

    Installed SQL Nuget Packages
    Created Initial Database file

---
+++ FinalProj_MobileApp/DB/LocalDBService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace FinalProj_MobileApp.DB
+{
+    internal class LocalDBService
+    {
+    }
+}
