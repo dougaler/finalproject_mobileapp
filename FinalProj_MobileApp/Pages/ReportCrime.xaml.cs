4db98cccbc5c3b13d493cb85760cd07abee14e9e
Zach Betsch <betschzt@mail.uc.edu>
2025-04-18 19:51:58 -0400

    Beginning Integration to main

--- FinalProj_MobileApp/Pages/ReportCrime.xaml.cs
+++ FinalProj_MobileApp/Pages/ReportCrime.xaml.cs
@@ -1,9 +1,19 @@
+using FinalProj_MobileApp.DB;
+
 namespace FinalProj_MobileApp.Pages;
 
 public partial class ReportCrime : ContentPage
 {
-	public ReportCrime()
+	private readonly LocalDBService _localDBService;
+
+	public ReportCrime(LocalDBService dBService)
 	{
 		InitializeComponent();
+		_localDBService = dBService;
 	}
+
+    private void btnSave_Clicked(object sender, EventArgs e)
+    {
+		if ()
+    }
 }
